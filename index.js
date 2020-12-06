9fc9fabd859731a96d091b76660cd70cd85a22d7
Reo Tamai <reotamai5@gmail.com>
2020-12-06 13:59:26 -0800

    common server detect

--- index.js
+++ index.js
@@ -10,5 +10,30 @@ client.login('Nzg1MTk0NTExNjY3NDI5NDA3.X80TXw.B-sZMaGaTRznmnaw1OPbteivJx8');
 client.on('message', message => {
   if (message.channel.type === 'dm') {
     client.channels.cache.get('785187351693492246').send(message.content);
+
+
+  let common = {};
+  let user_id = "262525851696889856";
+
+  client.users.fetch(user_id).then(function(res){
+    //console.log(res);
+    client.guilds.cache.forEach(guild => {
+      guild.members.fetch(user_id).then(function(){
+        guild.members.cache.each(member=>{
+          
+          if (member.user.id == user_id){
+            common[guild.id] = {
+              "serverID": guild.id,
+              "serverName": guild.name,
+              "userID": member.user.id,
+              "userName": member.user.username
+            }
+          }
+        });
+
+        console.log(common);
+      });
+    });
+  });
   }
 });
