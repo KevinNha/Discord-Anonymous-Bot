b585b76a728a8c9e3a001db4eb307f955e6a1743
Hyunjin Nha <hyunjinnha@gmail.com>
2020-12-09 00:16:24 -0800

    Hopefully finalized the async portion so the list of mutual servers pop up first message

--- anonbot.js
+++ anonbot.js
@@ -93,7 +93,7 @@ const getCommon = async () => {
   await client.guilds.cache.forEach(guild => {
     guild.members.fetch(senderID).then(_ => {
 
-      guild.members.cache.each(member => {
+      asyncguild.members.cache.each(async member => {
 
         if (member.user.id == senderID) {
           let toAdd = {
@@ -102,7 +102,7 @@ const getCommon = async () => {
             "userID": member.user.id,
             "userName": member.user.username
           };
-          addCommon(toAdd);
+          await addCommon(toAdd);
         }
       });
       if (lastGuild == guild) {
